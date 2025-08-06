e41ab4e070fa96c8814796d37280808cdb00181e
joeNWP01 <JoeW@nwp-inc.com>
2025-08-05 21:23:30 -0700

    First commit, main

--- week-1/openai-quickstart.py
+++ week-1/openai-quickstart.py
@@ -1,5 +1,7 @@
+from pydoc import text
 from openai import OpenAI
 from dotenv import load_dotenv
+from pydantic import BaseModel
 import os
 
 """
@@ -18,12 +20,31 @@ load_dotenv()
 
 client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 
+class BedTimeStory(BaseModel):
+    story: str
+    word_count: int
 # --------------------------------------------------------------
 # Generate a response
 # --------------------------------------------------------------
 
-response = client.responses.create(
-    model="gpt-4.1", input="Write a one-sentence bedtime story about a unicorn."
+# response = client.responses.create(
+#     model="gpt-4.1", input="Write a one-sentence bedtime story about a unicorn."
+# )
+
+response = client.responses.parse(
+    model="gpt-4.1",
+    input=[
+        {"role": "system", "content": "You are a bedtime storyteller. You will be given a topic and you will need to write a bedtime story about it and count the number of words in the story. The number of words in the story will exclude punctuation characters, blank space, and end-of-line characters"},
+        {
+            "role": "user",
+            "content": "Write a one-sentence bedtime story about a unicorn.",
+        },
+    ],
+    text_format=BedTimeStory,
 )
 
-print(response.output_text)
+story = response.output_parsed
+
+print(story.story)
+print("The number of words in this story is: ", story.word_count)
+
